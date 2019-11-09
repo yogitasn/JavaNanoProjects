4182fcbdb394de02aaba3badab92aeaaeee25dcf
Yogita Nesargi <yogitasn@yahoo.com>
2019-11-09 14:09:02 -0500

    Added code for splunk logging

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -15,9 +15,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class SareetaApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SareetaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
+
+		logger.error("Message logged at ERROR level");
+
+		logger.warn("Message logged at WARN level");
+
+		logger.info("Message logged at INFO level");
+
+		logger.debug("Message logged at DEBUG level");
 	}
 
 	@Bean
